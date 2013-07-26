e6d6c26cddd9f2555b45a6df61cf05a9881fca18
Jamal Powell <jamalkpowell@gmail.com>
2013-07-25 20:48:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inlaw::Application.config.secret_token = '6f8cf7ef94b827ad6e89875954997c7d0519e3ebac478ab55ce98831d42beae675acbecf67156e3abfa427f2b8c11b33de99b8535f67855881cae0d03f7af548'
